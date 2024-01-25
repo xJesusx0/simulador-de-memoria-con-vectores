c40e7bc099099e2e76cba300ef43f73c2077ad77
xJesusx0 <jesuspl0411@gmail.com>
2024-01-25 17:33:48 -0500

    Errores corregidos

--- src/tareavectormemoria/Metodos.java
+++ src/tareavectormemoria/Metodos.java
@@ -4,6 +4,85 @@ import java.util.Scanner;
 import static tareavectormemoria.TareaVectorMemoria.posicionInicialEnMemoria;
 
 public class Metodos {
+
+    public static void mostrarError(String mensaje) {
+        System.out.println("Error: " + mensaje);
+    }
+
+    public static String input(String mensaje) {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println(mensaje);
+            String UserInput = input.nextLine();
+
+            if (!UserInput.trim().isEmpty()) {
+                return UserInput;
+
+            } else {
+                mostrarError("La entrada esta vacia, intentelo denuevo");
+            }
+        }
+    }
+
+    public static int inputIntPositivo(String mensaje) {
+        while (true) {
+            String entrada = input(mensaje);
+
+            try {
+                int numero = Integer.parseInt(entrada);
+
+                if (numero > 0) {
+                    return numero;
+                } else {
+                    mostrarError("Ingrese un número mayor a 0, inténtelo de nuevo");
+                }
+
+            } catch (NumberFormatException e) {
+                mostrarError("Entrada inválida, inténtelo de nuevo");
+            }
+        }
+    }
+
+    public static int inputInt(String mensaje) {
+        while (true) {
+            String entrada = input(mensaje);
+
+            try {
+                return Integer.parseInt(entrada);
+
+            } catch (NumberFormatException e) {
+                mostrarError("Entrada invalida, intenelo denuevo");
+
+            }
+        }
+    }
+
+    public static double inputDouble(String mensaje) {
+        while (true) {
+            String entrada = input(mensaje);
+
+            try {
+                return Double.parseDouble(entrada);
+
+            } catch (NumberFormatException e) {
+                mostrarError("Entrada invalida, intentelo denuevo");
+
+            }
+        }
+    }
+
+    public static byte inputByte(String mensaje) {
+        while (true) {
+            String entrada = input(mensaje);
+            try {
+                return Byte.valueOf(entrada);
+
+            } catch (NumberFormatException e) {
+                mostrarError("Entrada invalida, intentelo denuevo");
+            }
+        }
+    }
+
     public static void imprimirVector(int[] vector) {
         String resultado = "";
         for (int i = 0; i < vector.length; i++) {
@@ -11,8 +90,8 @@ public class Metodos {
         }
         System.out.println(resultado);
     }
-    
-    public static int[] rellenarVector(int cantidadFilas,int cantidadColumnas,int[] vector){
+
+    public static int[] rellenarVector(int[] vector, int cantidadFilas, int cantidadColumnas) {
         for (int i = 0; i < cantidadFilas; i++) {
             for (int j = 0; j < cantidadColumnas; j++) {
                 vector[posicionInicialEnMemoria + cantidadColumnas * i + j] = inputInt("Ingrese el valor en la fila: " + i + " columna: " + j);
@@ -20,93 +99,74 @@ public class Metodos {
         }
         return vector;
     }
-    
-    public static int obtenerElemento(int i,int j,int cantidadColumnas,int[]vector){
-        return vector[posicionInicialEnMemoria + cantidadColumnas*i+j];
-    }
-    
-    public static void mostrarError(String mensaje){
-        System.out.println("Error: " + mensaje);
-    }  
-    public static String input(String mensaje){ 
-        Scanner input = new Scanner(System.in);
-        while (true) {            
-            System.out.println(mensaje);
-            String UserInput = input.nextLine();
 
-            if (!UserInput.trim().isEmpty()) {
-                return UserInput;
-                
-            } else {
-                mostrarError("La entrada esta vacia, intentelo denuevo");
-            }
-        }
+    public static int obtenerElemento(int[] vector, int i, int j, int cantidadColumnas) {
+        return vector[posicionInicialEnMemoria + cantidadColumnas * i + j];
     }
-    public static int inputInt(String mensaje){
-        while(true){
-        String entrada = input(mensaje);
-        
-            try {
-                return Integer.parseInt(entrada);
-                
-            } catch (NumberFormatException e) {
-                mostrarError("Entrada invalida, intenelo denuevo");
 
-            }
-        }
-    }
-    public static double inputDouble(String mensaje){
-        while(true){
-        String entrada = input(mensaje);
-        
-            try {
-                return Double.parseDouble(entrada);
-                
-            } catch (NumberFormatException e) {
-                mostrarError("Entrada invalida, intentelo denuevo");
-
-            }
-        }
-    }
-    public static byte inputByte(String mensaje){
-        while(true){
-        String entrada = input(mensaje);
-        
-            try {
-                return Byte.valueOf(entrada);
-                
-            } catch (NumberFormatException e) {
-                mostrarError("Entrada invalida, intentelo denuevo");
-
-            }
-        }
-    }
-    
-    public static int sumatoriaColumna(int j, int cantidadFilas,int cantidadColumnas,int[]vector){
+    public static int sumatoriaColumna(int[] vector, int j, int cantidadFilas, int cantidadColumnas) {
         int sumatoria = 0;
-        for (int i = 0 ; i < cantidadFilas ; i++){
-            sumatoria += obtenerElemento(i, j, cantidadColumnas, vector);
+        for (int i = 0; i < cantidadFilas; i++) {
+            sumatoria += obtenerElemento(vector, i, j, cantidadColumnas);
         }
         return sumatoria;
     }
-    
-    public static void menu(){
+
+    public static void menu() {
         System.out.println("-------------");
         System.out.println("0 - Salir");
         System.out.println("1 - Rellenar matriz");
-        System.out.println("2 - Mostrar memoria");
+        System.out.println("2 - Mostrar vector");
         System.out.println("3 - Mostrar matriz");
-        System.out.println("4 - Identificar la columna que mas suma");
+        System.out.println("4 - Ver todas las sumatorias");
+        System.out.println("5 - Ver la sumatoria mayor");
+        System.out.println("6 - Ver la sumatoria menor");
+        System.out.println("7 - Seleccionar 2 elementos de la matriz y compararlos");
     }
-    
-    public static void imprimirMatriz(int[] vector,int cantidadFilas,int cantidadColumnas){
-       String men = "";
+
+    public static void imprimirMatriz(int[] vector, int cantidadFilas, int cantidadColumnas) {
+        String men = "";
         for (int i = 0; i < cantidadFilas; i++) {
             for (int j = 0; j < cantidadColumnas; j++) {
-                men += (obtenerElemento(i, j, cantidadColumnas, vector)+ "\t"); 
+                men += (obtenerElemento(vector, i, j, cantidadColumnas) + "\t");
             }
             men += "\n";
         }
-         System.out.println(men);
+        System.out.println(men);
+    }
+
+    public static int[] obtenerSumatorias(int[] vector, int cantidadFilas, int cantidadColumnas) {
+        int sumatorias[] = new int[cantidadColumnas];
+        for (int j = 0; j < cantidadColumnas; j++) {
+            sumatorias[j] = sumatoriaColumna(vector, j, cantidadFilas, cantidadColumnas);
+
+        }
+        return sumatorias;
+    }
+
+    public static int[] obtenerMayor(int[] vector) {
+        int mayor = vector[0];
+        int indice = 0;
+        for (int i = 1; i < vector.length; i++) {
+            if (vector[i] > mayor) {
+                indice = i;
+                mayor = vector[i];
+            }
+        }
+        int datos[] = {mayor, indice};
+        return datos;
+    }
+
+    public static int[] obtenerMenor(int[] vector) {
+        int menor = vector[0];
+        int indice = 0;
+        for (int i = 1; i < vector.length; i++) {
+            if (vector[i] < menor) {
+                indice = i;
+                menor = vector[i];
+            }
+        }
+        int datos[] = {menor, indice};
+        return datos;
     }
 }
--- src/tareavectormemoria/TareaVectorMemoria.java
+++ src/tareavectormemoria/TareaVectorMemoria.java
@@ -3,15 +3,14 @@ package tareavectormemoria;
 import static tareavectormemoria.Metodos.*;
 
 public class TareaVectorMemoria {
-    public static int posicionInicialEnMemoria;
+    public static int posicionInicialEnMemoria = 0;
     
     public static void main(String[] args) {
 
-        posicionInicialEnMemoria = 0;
         boolean vectorRellenado = false;
         
-        int cantidadFilas = inputInt("Ingrese la cantidad de filas");
-        int cantidadColumnas = inputInt("Ingrese la cantidad de columnas");
+        int cantidadFilas = inputIntPositivo("Ingrese la cantidad de filas");
+        int cantidadColumnas = inputIntPositivo("Ingrese la cantidad de columnas");
 
         int vectorMemoria[] = new int[posicionInicialEnMemoria + cantidadFilas * cantidadColumnas];
 
@@ -20,29 +19,66 @@ public class TareaVectorMemoria {
             byte eleccion = inputByte("Ingrese la opcion que desea elegir");
             System.out.println("-------------");
             switch (eleccion) {
-                case 0:
-                    break;
+                case 0 -> {
                     
-                case 1:
+                }
+                    
+                case 1 -> {
                     if(!vectorRellenado){
-                        vectorMemoria = rellenarVector(cantidadFilas, cantidadColumnas,  vectorMemoria);
+                        vectorMemoria = rellenarVector(vectorMemoria,cantidadFilas, cantidadColumnas);
                         vectorRellenado = true;
                     } else {
                         mostrarError("El vector ya ha sido rellenado");
                     }
-                   break;
-                   
-                default:
-                    System.out.println("Opción no válida");               
+                }
+                case 2 -> {
+                    imprimirVector(vectorMemoria);
+                }
+                case 3 -> {
+                    imprimirMatriz(vectorMemoria,cantidadFilas,cantidadColumnas);
+                }
+                case 4 ->{
+                    if(vectorRellenado){
+                        int[] sumatorias = obtenerSumatorias(vectorMemoria, cantidadFilas, cantidadColumnas);
+                        imprimirVector(sumatorias);
+                    } else {
+                        mostrarError("El vector no ha sido rellenado");
+                    }
+                }
+                case 5 ->{
+                    if(vectorRellenado){
+                        int[] sumatorias = obtenerSumatorias(vectorMemoria, cantidadFilas, cantidadColumnas);
+                        //El resultado es un vector que contiene el elemento mayor y su indice
+                        int resultado[] = obtenerMayor(sumatorias);
+                        int elementoMayor = resultado[0];
+                        int indice = resultado[1];
+                        System.out.println("La sumatoria mayor fue de " + elementoMayor + " en la posicion " + indice);
+                    } else {
+                        mostrarError("El vector no ha sido rellenado");
+                                       }
+                }
+                case 6 ->{
+                    if(vectorRellenado){
+                        int[] sumatorias = obtenerSumatorias(vectorMemoria, cantidadFilas, cantidadColumnas);
+                        //El resultado es un vector que contiene el elemento mayor y su indice
+                        int resultado[] = obtenerMenor(sumatorias);
+                        int elementoMenor = resultado[0];
+                        int indice = resultado[1];
+                        System.out.println("La sumatoria menor fue de " + elementoMenor + " en la posicion " + indice);
+                    } else {
+                        mostrarError("El vector no ha sido rellenado");
+                                       }
+                }
+                case 7 ->{
+                    
+                }
+                default -> System.out.println("Opción no válida");               
             }
-            System.out.println("-------------");
+                        
             if(eleccion == 0 ){
                 break;
             }
-            imprimirVector(vectorMemoria);
-            System.out.println(sumatoriaColumna(0,  cantidadFilas, cantidadColumnas, vectorMemoria));
 
         }
-
     }
 }
